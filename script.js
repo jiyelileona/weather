2011a6dade443c93048ccc3f09a745ba56547b26
Jiye Li <15832778446@163.com>
2021-01-05 10:06:30 -0600

    get current weather information

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+import regeneratorRuntime from 'regenerator-runtime';
+require('dotenv').config();
+
 const getLocation = () => {
   if ('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(position => {
       let long = position.coords.longitude;
       let lat = position.coords.latitude;
-     
+      getCurrentConditions(long, lat);
     });
   } else {
     document.querySelector('h2').innerHTML =
@@ -11,5 +14,16 @@ const getLocation = () => {
   }
 };
 
+const getCurrentConditions = async (long, lat) => {
+  try {
+    const res = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${process.env.API_KEY}`
+    );
+    const data = await res.json();
+
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 window.addEventListener('load', getLocation);
